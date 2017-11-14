b616ffdfb98375fee420f0c6b2bdc4f29366855b
Dartanian Bagby <mighty.chess@gmail.com>
2017-11-14 11:40:46 -0800

    hih

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
+
+var app = express();
+var PORT = 8080;
+
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
