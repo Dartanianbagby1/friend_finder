528042e5aa47ba585f2e73183dca3eb566e0be9b
Dartanian Bagby <mighty.chess@gmail.com>
2017-11-17 09:58:39 -0800

    hi

--- server.js
+++ server.js
@@ -4,7 +4,63 @@ var path = require("path");
 
 
 var app = express();
-var PORT = 8080;
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//======================================
+
+var friends =[{	
+	routeName:"dartanian",
+	name: "dartanian",
+	score:[5, 4, 3, 2, 1]
+}, {
+	routeName:"woky",
+	name:"sleepy",
+	score:[5, 4, 3, 2, 1]
+}]; 
+ 
+//======================================
+app.get("/",function(req,res){
+
+	res.sendFile(path.join(__dirname,"view.html"));
+});
+app.get("/add", function(req, res){
+	res.sendFile(path.join(__dirname,"add.html"));
+
+	app.get("/all", function(req,res){
+		res.json(friends)
+	})
+})
+app.get("/api/:friends?", function(req,res){
+	var choice = req.params.friends;
+	if(choice){
+		console.log(choice);
+	  for (var i = 0; i < friends.length; i++) {
+	  	if (choice === friends[i].routeName) {
+	  		return res.json(friends[i]);
+	  	}
+	  }
+	  return res.send("None of friends found");
+	}
+	return res.json(friends);
+})
+app.post("/api/new",function(req,res){
+	var newfreinds =req.body;
+	console.log(newfreinds);
+	friends.push(newfreinds);
+	res.json(newfreinds);
+
+});
+//app.get("/add", function(req, res) {
+//  res.sendFile(path.join(__dirname, "add.html"));
+//});
+
+//app.get("/:choice", function(req, res) {
+//	var choice = req.params.choice;
+//	console.log(choice);
+//	res.end();
+//	});
 
 
 
