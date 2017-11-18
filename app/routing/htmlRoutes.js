453b4f86184c2d1056a91ce28cbd9e1b129afdbc
Dartanian Bagby <mighty.chess@gmail.com>
2017-11-17 18:02:36 -0800

    hi

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,14 @@
 var path = require("path");
 
-app.get("/", function(req, res){
+ module.exports = function(path) {
+	app.get("/", function(req, res){
 	res.sendFile(path.join(__dirname,"/../public/home.html"));
 });
 app.get("/survey", function(req, res){
 	res.sendFile(path.join(__dirname,"/../public/survey.html"));
 
+}
 
-	module.exports = path;
+
+
+	
