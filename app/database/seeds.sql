d0d9a713b0004081a4aa5cc4cdcd32ccc6bc2a95
Dartanian Bagby <mighty.chess@gmail.com>
2017-11-19 11:32:21 -0800

    ji

---
+++ app/database/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES ('Charles McDowell','https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/14224967_358092211189516_532114160343420505_n.jpg?oh=0aaf2d8540b74d31423577d05f422fc4&oe=5A4AAC19',1,5,1,1,1,1,5,5,1,5);
